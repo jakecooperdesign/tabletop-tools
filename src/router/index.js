8a544070898aa6b91fbaf825dccea04719a3b460
jakecooperdesign <jakecooperdesign@gmail.com>
2020-10-05 20:37:45 -0400

    changed header

--- src/router/index.js
+++ src/router/index.js
@@ -7,15 +7,10 @@ const routes = [
     path: '/',
     name: 'Home',
     component: InitiativeTracker,
+    meta: {
+      title: 'Tabletop Tools - Initiative Tracker',
+    }
   },
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  // },
   // {
   //   path: '/initiative-tracker',
   //   name: 'Initiative Tracker',
