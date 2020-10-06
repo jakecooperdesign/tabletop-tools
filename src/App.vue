68ade1a1330bf63bc1c28876798c9665d845a3fc
jakecooperdesign <jakecooperdesign@gmail.com>
2020-10-05 20:21:26 -0400

    renamed

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,27 @@
 <template>
   <header class="bg-black text-white">
     <div class="container mx-auto py-8 px-12 flex justify-between items-center">
-      <h1 class="font-bold text-2xl">D&amp;D Tools</h1>
+      <h1 class="font-bold text-2xl">{{name}}</h1>
       <nav id="nav" class="text-sm space-x-2">
-        <router-link class="px-3 py-2 bg-gray-800 rounded hover:bg-gray-600 transition duration-200" to="/">Home</router-link>
+        <!-- <router-link class="px-3 py-2 bg-gray-800 rounded hover:bg-gray-600 transition duration-200" to="/">Home</router-link> -->
         <router-link class="px-3 py-2 bg-gray-800 rounded hover:bg-gray-600 transition duration-200" to="/initiative-tracker">Initiative Tracker</router-link>
-        <router-link class="px-3 py-2 bg-gray-800 rounded hover:bg-gray-600 transition duration-200" to="/about">About</router-link>
+        <!-- <router-link class="px-3 py-2 bg-gray-800 rounded hover:bg-gray-600 transition duration-200" to="/about">About</router-link> -->
       </nav>
     </div>
   </header>
   <router-view />
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      name: "Tabletop Tools"
+    }
+  }
+}
+</script>
+
 <style>
+
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../views/Home.vue';
+// import Home from '../views/Home.vue';
+import InitiativeTracker from '../views/InitiativeTracker.vue';
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home,
+    component: InitiativeTracker,
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  },
-  {
-    path: '/initiative-tracker',
-    name: 'Initiative Tracker',
-    component: () => import(/* webpackChunkName: "InitiativeTracker" */ '../views/InitiativeTracker.vue'),
-  },
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+  // },
+  // {
+  //   path: '/initiative-tracker',
+  //   name: 'Initiative Tracker',
+  //   component: () => import(/* webpackChunkName: "InitiativeTracker" */ '../views/InitiativeTracker.vue'),
+  // },
 ];
 
 const router = createRouter({
